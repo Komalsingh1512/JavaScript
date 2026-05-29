a40d13ef800ac6871fc3d9b977880adb85e622ef
Komal Singh <115030881+Komalsingh1512@users.noreply.github.com>
2026-05-29 15:00:48 +0530

    added moon

--- model window/script.js
+++ model window/script.js
@@ -9,7 +9,6 @@ const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
-
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
